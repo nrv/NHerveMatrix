ac810f6dea6bf72889ecea4ebcd29ff51e3ea03f
Nicolas HERVE <n.herve@laposte.net>
2011-07-05 16:41:38 +0200

    First commit

---
+++ NHerveMatrix/src/plugins/nherve/matrix/NHerveMatrix.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2010, 2011 Institut Pasteur.
+ * 
+ * This file is part of NHerveMatrix, which is an ICY plugin.
+ * 
+ * NHerveMatrix is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NHerveMatrix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NHerveMatrix. If not, see <http://www.gnu.org/licenses/>.
+ */
+package plugins.nherve.matrix;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+/**
+ * The Class NHerveMatrix.
+ * 
+ * @author Nicolas HERVE - nicolas.herve@pasteur.fr
+ */
+public class NHerveMatrix extends Plugin implements PluginLibrary {
+
+}
